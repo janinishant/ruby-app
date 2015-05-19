0a7363fbba5d91f62550ec7a9039ad63b95ffc56
= <=>
2015-05-18 18:03:01 -0700

    Rendering small dynamic pages by rails

---
+++ sample_app/app/controllers/pages_controller_spec.rb
@@ -0,0 +1,20 @@
+require_relative '../../spec/spec_helper'
+
+describe 'PagesController' do
+
+  describe "GET 'home'" do
+    it "should be successful" do
+      get 'home'
+      response.should be_success
+    end
+
+  end
+
+  describe "GET 'contact'" do
+    it "should be successful" do
+      get 'contact'
+      response.should be_success
+    end
+
+  end
+end
